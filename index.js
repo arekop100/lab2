b7117114b33232e9eee2924bb530ddac36f34f2c
arekop100 <arekop100@gmial.com>
2019-10-10 12:17:18 +0200

    first commint

---
+++ index.js
@@ -0,0 +1,12 @@
+﻿var _ = require('lodash');
+
+function sum() {
+    for (var i = 0; i < arguments.length; i++) {
+        if (typeof arguments[i] !== 'number') return new Error(i + '.Invalid variable type ' + typeof arguments[i])
+    }
+
+    var suma = _.mean(arguments)
+    console.log(suma)
+}
+
+sum(2, 4, 6)
