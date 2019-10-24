94b201141278f289f5d501725d6a17c8e3f14cfd
arekop100 <@Szybowiec1>
2019-10-24 12:56:08 +0200

    lab_2_zadania_full

--- index.js
+++ index.js
@@ -32,3 +32,80 @@ function max() {
 sum(2, 4, 6)
 min(2, 4, 6)
 max(2, 4, 6)
+
+
+
+
+const user = {
+    name: 'Imie',
+    "surname": 'Nazwisko',
+    "allGrades": [
+        {
+            "subjectName": 'Name1',
+            "grades": [5, 4, 3, 5, 2],
+            "weight": 3
+        },
+        {
+            "subjectName": 'Name2',
+            "grades": [3, 3.5, 2],
+            "weight": 1
+        },
+        {
+            "subjectName": 'Name3',
+            "grades": [4, 3, 3.5],
+            "weight": 5
+        }
+    ]
+};
+
+function srednia(ob){
+    const imie = ob.name;
+    const nazwisko = ob.surname;
+    const myArray = ob.allGrades
+    let licznik = 0;
+    let mianownik = 0;
+    let tmp = 0;
+    //console.log(obiekt.allGrades[0].grades);
+    for(const el of ob.allGrades){
+        tmp += _.reduce(el.grades,function(sum,n){
+            return sum+n;
+        },0);
+        licznik += (tmp*el.weight);
+        mianownik += (el.weight * el.grades.length);
+        tmp = 0;
+    }
+    return licznik/mianownik;
+}
+
+
+let z = srednia(user);
+console.log("Zadanie 3: "+z);
+
+const collections = [
+    {},
+    15,
+    "hello@test.pl",
+    null,
+    ['aaa', 'bbb', 5],
+    'admin@gmail.com',
+    undefined,
+    'a34@yahoo.com',
+    '321@a',
+    '321.pl'
+];
+
+
+function getMails(ob){
+    const reg = new RegExp(/^\S+@\S+\.[A-Za-z]+$/);
+    let myArray = [];
+    for(const x of ob){
+        if(typeof x === "string"){
+            if(reg.test(x)){
+                myArray.push(x);
+            }
+        }
+    }
+    return myArray.sort();
+}
+let k = getMails(collections);
+console.log("Zadanie 4: "+k);
